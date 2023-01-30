2c27666c23193324378a0a06ffffdb23439bb6c7
maouia <maaouia0@gmail.com>
2023-01-30 13:41:00 +0100

    interface and clean code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeadermobileComponent } from './headermobile/headermobile.component';
+import { UserpanelComponent } from './userpanel/userpanel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeadermobileComponent,
+    UserpanelComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/headermobile/headermobile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-headermobile',
+  templateUrl: './headermobile.component.html',
+  styleUrls: ['./headermobile.component.css']
+})
+export class HeadermobileComponent {
+
+}
